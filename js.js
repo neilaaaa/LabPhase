07399d01e4fb776bb1d0a6c7133bda19e4c6e8f7
Neilaa <your-email@example.com>
2024-12-04 22:12:40 +0100

    Initial commit

---
+++ js.js
@@ -0,0 +1,9 @@
+document.getElementById("openNewPage") .addEventListener("click", function () {
+  window.location.href= "main2.html";
+});
+document.getElementById("openNewPage2") .addEventListener("click", function () {
+  window.location.href= "main3.html";
+});
+document.getElementById("openNewPage3") .addEventListener("click", function () {
+  window.location.href= "main4.html";
+});
